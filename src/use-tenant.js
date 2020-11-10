9d8d83b4e1da5420fad8bbfe3d4e03f3a0478e5d
Brandon Bakker <brandon@bdmd.com.au>
2020-11-10 13:10:43 +1000

    Add working module reset

--- src/use-tenant.js
+++ src/use-tenant.js
@@ -1,8 +1,5 @@
 import { testTenant } from "./tenant"
 
 export const useTenant = () => {
-  it('should make a unique test id', async () => {
-    const myTenant = testTenant
-    console.log(myTenant.id)
-  })
+  return testTenant.id
 }
--- src/use-tenant.test.js
+++ src/use-tenant.test.js
@@ -1,10 +1,25 @@
-import { useTenant } from './use-tenant'
-
 describe('test ids', () => {
-  useTenant()
-  useTenant()
-  useTenant()
-  useTenant()
-  useTenant()
-  useTenant()
+  beforeEach(() => {
+    jest.resetModules()
+  })
+
+  it('test id 1', async () => {
+    const { useTenant } = require('./use-tenant');
+    console.log(useTenant())
+  })
+
+  it('test id 2', async () => {
+    const { useTenant } = require('./use-tenant');
+    console.log(useTenant())
+  })
+
+  it('test id 3', async () => {
+    const { useTenant } = require('./use-tenant');
+    console.log(useTenant())
+  })
+
+  it('test id 4', async () => {
+    const { useTenant } = require('./use-tenant');
+    console.log(useTenant())
+  });
 })
